abeabcbdc16bfe75fe4831d01bc9b6a59cbeec1a
humzamanzoor <humzamanzur@gmail.com>
2026-06-08 18:04:27 +0300

    initial commit

---
+++ k6-grpc.js
@@ -0,0 +1,39 @@
+import grpc from 'k6/net/grpc';
+import { sleep } from 'k6';
+import http from 'k6/http';
+
+const BACKEND = 'odroid2:5000';
+const PG_HOST = 'http://controller.lan:8080';
+const SESSION_ID = __ENV.SESSION_ID;
+const IMPLEMENTATION = __ENV.IMPLEMENTATION;
+
+const client = new grpc.Client();
+client.load(['.'], 'user_service.proto');
+
+export const options = {
+    vus: 5,
+    duration: '30s',
+};
+
+export function setup() {
+    http.get(`${PG_HOST}/api/v2/session/${SESSION_ID}/measurement/start/CLIENT/${IMPLEMENTATION}`);
+    http.get(`${PG_HOST}/api/v2/session/${SESSION_ID}/run/start/CLIENT/1`);
+}
+
+export default function () {
+    client.connect(BACKEND, { plaintext: true });
+    
+    client.invoke('UserService/GetUserWithPostsAndComments', {
+        user_id: 1,
+        posts_limit: 10,
+        comments_limit: 10
+    });
+    
+    client.close();
+    sleep(1);
+}
+
+export function teardown() {
+    http.get(`${PG_HOST}/api/v2/session/${SESSION_ID}/run/stop/CLIENT/1`);
+    http.get(`${PG_HOST}/api/v2/session/${SESSION_ID}/measurement/stop/CLIENT/${IMPLEMENTATION}`);
+}
---
+++ run.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+HOST=$1
+SESSION=$2
+RUNS=$3
+
+k6 run -e SESSION_ID=$SESSION -e IMPLEMENTATION=Grpc.AspNetCore.C20 /home/utu/humza/rq3-grpc/k6-grpc.js
