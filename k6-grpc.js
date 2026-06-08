74636fece3358e0fbdd8e1273a369d19e763094f
humzamanzoor <humzamanzur@gmail.com>
2026-06-08 18:39:42 +0300

    grpc - C30

--- k6-grpc.js
+++ k6-grpc.js
@@ -11,7 +11,7 @@ const client = new grpc.Client();
 client.load(['.'], 'user_service.proto');
 
 export const options = {
-    vus: 25,
+    vus: 30,
     duration: '30s',
 };
 
--- measurement.md
+++ measurement.md
@@ -1,5 +1,5 @@
 ---
-name: gRPC.C25
+name: gRPC.C30
 author: Humza
 description: Energy consumption measurement of gRPC.ASPNetCore under concurrent load using k6.
 active-nodes:
